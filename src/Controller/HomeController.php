b12f8da505a061bac6b2f41e7e43d3b2fa231018
Mat911 <matisse.demontis@gmail.com>
2024-11-23 08:48:12 +0400

    ajout d'une partie du dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\MissionRepository;
+use App\Repository\SuperHeroRepository;
+use App\Repository\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
+    public function index(
+        MissionRepository $missionRepository,
+        SuperHeroRepository $heroRepository,
+        TeamRepository $teamRepository
+    ): Response {
+        $now = new \DateTime();
+
+        $activeMissions = $missionRepository->createQueryBuilder('m')
+            ->where('m.startAt <= :now')
+            ->andWhere('m.endAt >= :now')
+            ->setParameter('now', $now)
+            ->getQuery()
+            ->getResult();
+
+        $availableHeroes = $heroRepository->findBy(['isAvailable' => true]);
+        $unavailableHeroes = $heroRepository->findBy(['isAvailable' => false]);
+
+        $teams = $teamRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'active_missions' => $activeMissions,
+            'available_heroes' => $availableHeroes,
+            'unavailable_heroes' => $unavailableHeroes,
+            'teams' => $teams,
+            'missions_count' => count($activeMissions),
+            'heroes_count' => count($availableHeroes) + count($unavailableHeroes),
+            'teams_count' => count($teams),
+        ]);
     }
 }
